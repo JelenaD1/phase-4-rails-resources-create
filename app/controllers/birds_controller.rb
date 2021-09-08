29c2326bd16cd2c55f50191c029239ff11c783ff
Jelena <jelena.durovic@powerhrg.com>
2021-09-08 14:12:50 -0400

    commit tree 05bb616224a2e2434b808211d398ef16c90b4dc9

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,14 @@
 class BirdsController < ApplicationController
 
+  def create
+
+    #get the data from the body of the request
+    #create a new Bird
+    bird = Bird.create(name: params[:name], species: params[:species])
+    #send the newly created Bird as response
+    render json: bird, status: :created
+  end 
+
   # GET /birds
   def index
     birds = Bird.all
@@ -15,5 +24,7 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  
 
 end
